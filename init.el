a2bed869152fdd3bc09f46f49c9d6d94ec097861
Andres <andresnrx@gmail.com>
2016-09-21 00:25:42 -0700

    added helm

--- init.el
+++ init.el
@@ -16,15 +16,20 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (require 'color-theme)
-    (color-theme-initialize)
-    (color-theme-midnight)
+(color-theme-initialize)
+(color-theme-midnight)
+
+(require 'helm-config)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (helm racer flycheck-rust company color-theme cargo async))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
