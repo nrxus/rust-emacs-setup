80a3684978a0a3f3612be1f2f7b715baf689fdd5
Andres <andresnrx@gmail.com>
2016-10-30 20:13:42 -0700

    added undo, racer

--- init.el
+++ init.el
@@ -6,18 +6,22 @@
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
+(show-smartparens-global-mode 1)
+(smartparens-global-mode 1)
+
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
 (add-hook 'rust-mode-hook
           (lambda ()
             (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+(sp-local-pair 'rust-hook-mode "'" nil)
 
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-midnight)
+(color-theme-charcoal-black)
 
 (require 'helm)
 (require 'helm-config)
@@ -26,9 +30,7 @@
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
-(define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
-(define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
-(define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
+(global-undo-tree-mode)
 
 (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
@@ -39,13 +41,18 @@
       helm-buffers-fuzzy-matching t          ; fuzzy matching buffer names when non-nil
       helm-echo-input-in-header-line t)
 
-(setq helm-autoresize-max-height 0)
-(setq helm-autoresize-min-height 20)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)(global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (helm-autoresize-mode 1)
 (add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq racer-cmd "~/.cargo/bin/racer")
+;;(setq racer-rust-src-path "/home/nrzus/Programs/rust/src/")
+
+;;(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
 
 (helm-mode 1)
 
@@ -57,7 +64,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (helm racer flycheck-rust company color-theme cargo async))))
+    (smartparens undo-tree helm racer flycheck-rust company color-theme cargo async))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
