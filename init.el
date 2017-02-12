be4815db8fee5c42a31a3d76ba50e173be54e6e0
Andres <andresnrx@gmail.com>
2017-02-12 01:08:03 -0800

    updating packages

--- init.el
+++ init.el
@@ -10,6 +10,9 @@
 (show-smartparens-global-mode 1)
 (smartparens-global-mode 1)
 
+(require 'flymake-yaml) ;; Not necessary if using ELPA package
+(add-hook 'yaml-mode-hook 'flymake-yaml-load)
+
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
 (add-hook 'rust-mode-hook
@@ -20,9 +23,9 @@
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-charcoal-black)
+;;(require 'color-theme)
+;;(color-theme-initialize)
+;;(color-theme-charcoal-black)
 
 (require 'helm)
 (require 'helm-config)
@@ -49,7 +52,7 @@
 (add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq racer-cmd "~/.cargo/bin/racer")
-(setq racer-rust-src-path "/home/nrzus/Programs/rust/src/")
+(setq racer-rust-src-path (expand-file-name (getenv "RUST_SRC_PATH")))
 
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
@@ -60,15 +63,21 @@
 
 (helm-mode 1)
 
+(load-theme 'base16-atelier-plateau t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("542e6fee85eea8e47243a5647358c344111aa9c04510394720a3108803c8ddd1" "ffac21ab88a0f4603969a24b96993bd73a13fe0989db7ed76d94c305891fad64" "41eb3fe4c6b80c7ad156a8c52e9dd6093e8856c7bbf2b92cc3a4108ceb385087" "fc7fd2530b82a722ceb5b211f9e732d15ad41d5306c011253a0ba43aaf93dccc" "3e34e9bf818cf6301fcabae2005bba8e61b1caba97d95509c8da78cff5f2ec8e" "cabc32838ccceea97404f6fcb7ce791c6e38491fd19baa0fcfb336dcc5f6e23c" "1d079355c721b517fdc9891f0fda927fe3f87288f2e6cc3b8566655a64ca5453" "92192ea8f0bf04421f5b245d906701abaa7bb3b0d2b3b14fca2ee5ebb1da38d8" "760ce657e710a77bcf6df51d97e51aae2ee7db1fba21bbad07aab0fa0f42f834" "b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "34ed3e2fa4a1cb2ce7400c7f1a6c8f12931d8021435bad841fdc1192bd1cc7da" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" "6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" "aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" default)))
+ '(helm-autoresize-mode t)
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (helm-projectile smartparens undo-tree helm racer flycheck-rust company color-theme cargo async))))
+    (yaml-mode flymake-yaml rust-mode base16-theme helm-projectile smartparens undo-tree helm racer flycheck-rust company color-theme cargo async))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
