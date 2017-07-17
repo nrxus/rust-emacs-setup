c271cb27d7bed2f2185a556b004a68238a43d527
Andres <andresnrx@gmail.com>
2017-07-16 18:45:39 -0700

    remove elpa

--- elpa/rust-playground-20170211.5/rust-playground-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; rust-playground-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "rust-playground" "rust-playground.el" (22892
-;;;;;;  4408 140668 354000))
-;;; Generated autoloads from rust-playground.el
-
-(autoload 'rust-playground "rust-playground" "\
-Run playground for Rust language in a new buffer.
-
-\(fn)" t nil)
-
-(autoload 'rust-playground-rm "rust-playground" "\
-Remove files of the current snippet together with directory of this snippet.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; rust-playground-autoloads.el ends here
--- elpa/rust-playground-20170211.5/rust-playground-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rust-playground" "20170211.5" "Local Rust playground for short code snippets." '((emacs "24.3") (rust-mode "0.3.0")) :commit "ff4149489c30a65817750428847217368bd995ba" :url "https://github.com/grafov/rust-playground" :keywords '("tools" "rust"))
