2cdb43004cd189ad29550e9d82ed23fe2d958a13
Andres <andresnrx@gmail.com>
2018-11-04 02:50:20 -0800

    removed compat block; this already does not work for emacs below 24

--- init.el
+++ init.el
@@ -1,9 +1,8 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
 
